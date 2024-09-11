0f26fade339ede46f9e36d81f8866e879cb4dd26
Le Thanh Loc <lochcmus@gmail.com>
2024-09-11 13:59:29 +0700

    fix(github_action): test run github action when push code

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -10,17 +10,31 @@ from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
-    """Django command to wait for database"""
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
         """Entrypoint for command."""
-        self.stdout.write('Waiting for database...')
-        db_conn = None
-        while not db_conn:
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while db_up is False:
             try:
-                db_conn = connections['default']
-                db_conn.cursor()
-            except OperationalError:
+                self.check(database=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
-
         self.stdout.write(self.style.SUCCESS('Database available!'))
+
+# class Command(BaseCommand):
+#     """Django command to wait for database"""
+#     def handle(self, *args, **kwargs):
+#         """Entrypoint for command."""
+#         self.stdout.write('Waiting for database...')
+#         db_conn = None
+#         while not db_conn:
+#             try:
+#                 db_conn = connections['default']
+#                 db_conn.cursor()
+#             except OperationalError:
+#                 self.stdout.write('Database unavailable, waiting 1 second...')
+#                 time.sleep(1)
+
+#         self.stdout.write(self.style.SUCCESS('Database available!'))
